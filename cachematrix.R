b024cc4f72048c49d4e1618727095ecc74bb4c69
Wafa <wafa.adeel295@gmail.com>
2017-09-27 09:21:07 +0530

    Committing cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion and caching results
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+CacheMat <- function(x = matrix()) {
+  X<- NULL
+  set <- function(y){
+    x<<-y
+    X<<-NULL
+  }
+  get <- function()x
+  setX <- function(XX) X<<-XX
+  getX<- function() X
+  list(set=set, get=get, setX=setX, getX=getX)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  X <- x$getX()
+  if(!isnull(X)){
+    message("getting cached data")
+    return (X)
+  }
+  data <- x$get()
+  XX <- solve(data, ...)
+  x$setX(XX)
+  XX
+  
 }
